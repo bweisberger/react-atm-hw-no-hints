d8c22bcbbfead0d74c0e21429a3c11174d83c6f4
William Weisberger <bill.weisberger@gmail.com>
2019-08-06 09:45:26 -0600

    Finished everything but the last question - apparently throwing infinite loops, but not sure why.

--- src/Account/index.js
+++ src/Account/index.js
@@ -2,14 +2,36 @@ import React, { Component } from 'react';
 
 
 class Account extends Component {
+  constructor(){
+    super();
+    this.state = {
+      balance: 0,
+      balanceClass: ""
+    }
+  }
+  depositClick = (e) =>{
+    this.setState({balance: parseInt(this.state.balance + this.refs.inputRef.value)})
+  }
+  withdrawClick = (e) =>{
+    const newBalance = parseInt(this.state.balance - this.refs.inputRef.value);
+    if(newBalance >= 0){
+      this.setState({balance: parseInt(this.state.balance - this.refs.inputRef.value)})
+    } else {
+      alert("Insufficient Funds");
+    }
+  }
+
   render() {
+    if(this.state.balance === 0){
+      this.setState({balanceClass: `zero`})
+    }
     return (
-      <div className="account">
-        <h2>TODO: CHANGE TO PROP</h2>
-        <div className="balance">$0</div>
-        <input type="text" placeholder="enter an amount" />
-        <input type="button" value="Deposit" />
-        <input type="button" value="Withdraw" />
+      <div className={"account " + this.state.balanceClass}>
+        <h2>{this.props.name}</h2>
+        <div className="balance">{this.state.balance}</div>
+        <input type="text" ref='inputRef' placeholder="enter an amount" />
+        <input type="button" value="Deposit" onClick={this.depositClick}/>
+        <input type="button" value="Withdraw" onClick={this.withdrawClick} />
       </div>
     )
   }
